4d15ba6ac4c45315f2ea6620be90fe5952dcbc50
Glorified-Toaster <alhassanoday.ffxiv@gmail.com>
2025-11-02 22:45:54 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Glorified-Toaster/final-fantasy-term-go
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
+
+// TODO:
+//
+// DONE: make json file for names and ff part.
+// Draw the character by name
+// Draw by random
+// DONE: list all of the characters
+//
+
+type Sprite struct {
+	Name string `json:"name"`
+	FF   string `json:"ff"`
+}
+
+var (
+	sprite     []Sprite
+	spriteList string = "sprite.json"
+	program    string
+	programDir string
+	spriteDir  string
+
+	// Flags
+	listAll      *bool   = flag.Bool("a", false, "List all the sprites")
+	debugPath    *bool   = flag.Bool("x", false, "Enable debug mode")
+	selectByName *string = flag.String("name", "", "Select sprite by name")
+	selectRandom *bool   = flag.Bool("r", false, "Select random sprites")
+)
+
+func main() {
+	data, err := os.ReadFile(spriteList)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = json.Unmarshal(data, &sprite); err != nil {
+		panic(err)
+	}
+
+	program, err = os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	programDir = filepath.Dir(program)
+	spriteDir = filepath.Join(programDir, "SpritesDir")
+
+	flag.Parse()
+
+	switch {
+	case *debugPath:
+		PrintDebugPath(program, programDir, spriteDir)
+
+	case *listAll:
+		ListAllSprites(sprite)
+
+	case *selectRandom:
+		DrawByRandom(sprite)
+
+	case *selectByName != "":
+		DrawByName(*selectByName, sprite)
+
+	default:
+		println("Usage : ./go-open flag  -h : for help")
+	}
+}
+
+func DrawASCII(artPath string) {
+	if artPath == "" {
+		log.Fatal("Invalid Path")
+	}
+	sprite, err := os.ReadFile(artPath)
+	if err != nil {
+		log.Fatal("cannot open file")
+		return
+	}
+	fmt.Println(string(sprite))
+}
+
+func DrawByName(name string, sprites []Sprite) {
+	for _, sprite := range sprites {
+		if name == sprite.Name {
+			path := filepath.Join(spriteDir, name)
+			DrawASCII(path)
+			return
+		}
+	}
+	log.Println("Invalid name or non-existent sprite:", name)
+}
+
+func DrawByRandom(sprites []Sprite) {
+	if len(sprites) == 0 {
+		fmt.Println("No sprites available")
+		return
+	}
+
+	index := rand.Intn(len(sprites))
+	sprite := sprites[index]
+	path := filepath.Join(spriteDir, sprite.Name)
+	DrawASCII(path)
+}
+
+func ListAllSprites(sprites []Sprite) {
+	for i, sprite := range sprites {
+		fmt.Printf("%d - %s - %s\n", i, sprite.Name, sprite.FF)
+	}
+}
+
+func PrintDebugPath(program, programDir, spriteDir string) {
+	fmt.Println("PROGRAM:", program)
+	fmt.Println("PROGRAM_DIR:", programDir)
+	fmt.Println("SPRITE_DIR:", spriteDir)
+}
